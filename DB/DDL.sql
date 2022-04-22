ed3db782e3551353e9ba6dccd83fa8d4457de7f5
RDY00 <thescrocherstakss02@gmail.com>
2022-04-21 23:05:03 -0500

    DDL agregado

--- DB/DDL.sql
+++ DB/DDL.sql
@@ -1,289 +1,311 @@
 -- DROP DATABASE IF EXISTS cineforum;
 -- CREATE DATABASE cineforum;
+drop schema public cascade; create schema public;
 
--- USUARIOS
+-- USERS
 
-CREATE TABLE usuario (
-  alias VARCHAR (30) PRIMARY KEY,
-  biografia VARCHAR (280) CHECK (biografia <> ''),
-  correo VARCHAR (280) NOT NULL,
-  residencia VARCHAR (60),
-  publico BOOLEAN NOT NULL DEFAULT TRUE,
-  fecha_nacimiento DATE NOT NULL CHECK (fecha_nacimiento < CURRENT_DATE),
-  foto_perfil BYTEA NOT NULL,
-  contrasenia VARCHAR (60) NOT NULL CHECK (contrasenia <> ''),
-  nombre VARCHAR (60) NOT NULL CHECK (nombre <> ''),
-  paterno VARCHAR (60) NOT NULL CHECK (paterno <> ''),
-  materno VARCHAR (60) NOT NULL CHECK (materno <> ''),
-  eliminado BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE users (
+  username VARCHAR (30) PRIMARY KEY,
+  biography VARCHAR (280) CHECK (biography <> ''),
+  email VARCHAR (320) NOT NULL,
+  country VARCHAR (60),
+  is_public BOOLEAN NOT NULL DEFAULT TRUE,
+  date_of_birth DATE NOT NULL CHECK (date_of_birth < CURRENT_DATE),
+  picture BYTEA NOT NULL,
+  password VARCHAR (60) NOT NULL CHECK (password <> ''),
+  name VARCHAR (60) NOT NULL CHECK (name <> ''),
+  surname VARCHAR (60) NOT NULL CHECK (surname <> ''),
+  ssurname VARCHAR (60) NOT NULL CHECK (ssurname <> ''),
+  deleted BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE administrador (
-  alias VARCHAR (30) PRIMARY KEY,
-  FOREIGN KEY (alias)
-    REFERENCES usuario (alias)
+CREATE TABLE administrator (
+  username VARCHAR (30) PRIMARY KEY,
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE seguir_usuario (
-  alias_seguidor VARCHAR (30) NOT NULL,
-  alias_seguido VARCHAR (30) NOT NULL,
-  PRIMARY KEY (alias_seguidor, alias_seguido),
-  FOREIGN KEY (alias_seguidor)
-    REFERENCES usuario (alias)
+CREATE TABLE follow_user (
+  username_follower VARCHAR (30) NOT NULL,
+  username_following VARCHAR (30) NOT NULL,
+  PRIMARY KEY (username_follower, username_following),
+  FOREIGN KEY (username_follower)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (alias_seguido)
-    REFERENCES usuario (alias)
+  FOREIGN KEY (username_following)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT seguir_mismo_usuario
-    CHECK (alias_seguidor <> alias_seguido)
+  CONSTRAINT follow_same_user
+    CHECK (username_follower <> username_following)
 );
 
-CREATE TABLE reportar_usuario (
-  alias_reporta VARCHAR (30) NOT NULL,
-  alias_reportado VARCHAR (30) NOT NULL,
+CREATE TABLE report_user (
+  username_reporter VARCHAR (30) NOT NULL,
+  username_reportee VARCHAR (30) NOT NULL,
   motivo VARCHAR (60) NOT NULL CHECK (motivo <> ''),
   comentario TEXT CHECK (comentario <> ''),
-  PRIMARY KEY (alias_reporta, alias_reportado),
-  FOREIGN KEY (alias_reporta)
-    REFERENCES usuario (alias)
+  PRIMARY KEY (username_reporter, username_reportee),
+  FOREIGN KEY (username_reporter)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (alias_reportado)
-    REFERENCES usuario (alias)
+  FOREIGN KEY (username_reportee)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT reporta_mismo_usuario
-    CHECK (alias_reporta <> alias_reportado)
+  CONSTRAINT report_same_user
+    CHECK (username_reporter <> username_reportee)
 );
 
--- MENSAJES
+-- MESSAGES
 
-CREATE TABLE mensaje (
-  id_mensaje SERIAL PRIMARY KEY,
-  alias_usuario_rem VARCHAR (30) NOT NULL,
-  alias_usuario_dest VARCHAR (30) NOT NULL,
-  id_mensaje_respuesta INT,
-  contenido VARCHAR (500) NOT NULL CHECK (contenido <> ''),
-  fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (alias_usuario_rem)
-    REFERENCES usuario (alias)
+CREATE TABLE messages (
+  id_message SERIAL PRIMARY KEY,
+  username_sender VARCHAR (30) NOT NULL,
+  username_receiver VARCHAR (30) NOT NULL,
+  id_reply INT,
+  content VARCHAR (500) NOT NULL CHECK (content <> ''),
+  date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (username_sender)
+    REFERENCES users (username)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-  FOREIGN KEY (alias_usuario_dest)
-    REFERENCES usuario (alias)
+  FOREIGN KEY (username_receiver)
+    REFERENCES users (username)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_mensaje_respuesta)
-    REFERENCES mensaje (id_mensaje)
+  FOREIGN KEY (id_reply)
+    REFERENCES messages (id_message)
     ON DELETE RESTRICT
     ON UPDATE CASCADE
 );
 
--- ENTRADAS
+-- ENTRIES
 
-CREATE TABLE entrada (
-  id_entrada SERIAL PRIMARY KEY,
-  enlace VARCHAR (60) NOT NULL CHECK (enlace <> ''),
-  imagen BYTEA NOT NULL,
-  sinopsis VARCHAR (280) NOT NULL CHECK (sinopsis <> ''),
-  titulo VARCHAR (60) NOT NULL CHECK (titulo <> ''),
-  estreno DATE NOT NULL,
-  clasificacion VARCHAR (3) NOT NULL CHECK (clasificacion in ('AA', 'A', 'B', 'B15', 'C', 'D')),
-  tipo CHAR (1) NOT NULL CHECK (tipo IN ('p', 's', 'c'))
+CREATE TABLE entry (
+  id_entry SERIAL PRIMARY KEY,
+  link VARCHAR (60) NOT NULL CHECK (link <> ''),
+  image BYTEA NOT NULL,
+  synopsis VARCHAR (280) NOT NULL CHECK (synopsis <> ''),
+  title VARCHAR (60) NOT NULL CHECK (title <> ''),
+  release DATE NOT NULL,
+  clasification VARCHAR (3) NOT NULL CHECK (clasification in ('AA', 'A', 'B', 'B15', 'C', 'D')),
+  type CHAR (1) NOT NULL CHECK (type IN ('p', 's', 'c'))
 );
 
-CREATE TABLE pelicula (
-  id_pelicula INT PRIMARY KEY,
-  duracion INT NOT NULL CHECK (duracion > 0),
-  FOREIGN KEY (id_pelicula)
-    REFERENCES entrada (id_entrada)
+CREATE TABLE movie (
+  id_movie INT PRIMARY KEY,
+  length INT NOT NULL CHECK (length > 0),
+  FOREIGN KEY (id_movie)
+    REFERENCES entry (id_entry)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE serie (
-  id_serie INT PRIMARY KEY,
-  emision BOOLEAN NOT NULL,
-  FOREIGN KEY (id_serie)
-    REFERENCES entrada (id_entrada)
+CREATE TABLE series (
+  id_series INT PRIMARY KEY,
+  running BOOLEAN NOT NULL,
+  FOREIGN KEY (id_series)
+    REFERENCES entry (id_entry)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE capitulo (
-  id_capitulo INT PRIMARY KEY,
-  id_serie INT NOT NULL,
-  temporada INT NOT NULL CHECK (temporada >= 0),
-  no_cap INT NOT NULL CHECK (no_cap >= 0),
-  duracion INT NOT NULL CHECK (duracion > 0),
-  FOREIGN KEY (id_serie)
-    REFERENCES serie (id_serie)
+CREATE TABLE episode (
+  id_episode INT PRIMARY KEY,
+  id_series INT NOT NULL,
+  season INT NOT NULL CHECK (season >= 0),
+  no_ep INT NOT NULL CHECK (no_ep >= 0),
+  length INT NOT NULL CHECK (length > 0),
+  FOREIGN KEY (id_series)
+    REFERENCES series (id_series)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_capitulo)
-    REFERENCES entrada (id_entrada)
+  FOREIGN KEY (id_episode)
+    REFERENCES entry (id_entry)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
--- CELEBRIDADES
+-- CELEBRITIES
 
-CREATE TABLE celebridad (
-  id_celebridad SERIAL PRIMARY KEY,
-  nombre VARCHAR (100) NOT NULL CHECK (nombre <> ''),
-  biografia TEXT CHECK (biografia <> ''),
-  foto BYTEA
+CREATE TABLE celebrity (
+  id_celebrity SERIAL PRIMARY KEY,
+  name VARCHAR (100) NOT NULL CHECK (name <> ''),
+  biography TEXT CHECK (biography <> ''),
+  picture BYTEA
 );
 
-CREATE TABLE seguir_celebridad (
-  alias VARCHAR (30) NOT NULL,
-  id_celebridad INT NOT NULL,
-  PRIMARY KEY (alias, id_celebridad),
-  FOREIGN KEY (alias)
-    REFERENCES usuario (alias)
+CREATE TABLE follow_celebrity (
+  username VARCHAR (30) NOT NULL,
+  id_celebrity INT NOT NULL,
+  PRIMARY KEY (username, id_celebrity),
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_celebridad)
-    REFERENCES celebridad (id_celebridad)
+  FOREIGN KEY (id_celebrity)
+    REFERENCES celebrity (id_celebrity)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE trabajar (
-  id_celebridad INT NOT NULL,
-  id_entrada INT NOT NULL,
-  PRIMARY KEY (id_celebridad, id_entrada),
-  FOREIGN KEY (id_celebridad)
-    REFERENCES celebridad (id_celebridad)
+CREATE TABLE movie_role (
+  id_celebrity INT NOT NULL,
+  id_entry INT NOT NULL,
+  PRIMARY KEY (id_celebrity, id_entry),
+  FOREIGN KEY (id_celebrity)
+    REFERENCES celebrity (id_celebrity)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_entrada)
-    REFERENCES entrada (id_entrada)
+  FOREIGN KEY (id_entry)
+    REFERENCES entry (id_entry)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE rol (
-  id_celebridad INT NOT NULL,
-  id_entrada INT NOT NULL,
+CREATE TABLE roles (
+  id_celebrity INT NOT NULL,
+  id_entry INT NOT NULL,
   rol VARCHAR (60) NOT NULL CHECK (rol <> ''),
-  PRIMARY KEY (id_celebridad, id_entrada, rol),
-  FOREIGN KEY (id_celebridad, id_entrada)
-    REFERENCES trabajar (id_celebridad, id_entrada)
+  PRIMARY KEY (id_celebrity, id_entry, rol),
+  FOREIGN KEY (id_celebrity, id_entry)
+    REFERENCES movie_role (id_celebrity, id_entry)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
--- LISTAS DE REPRODUCCION
+-- LISTS
 
-CREATE TABLE lista_reproduccion (
-  nombre VARCHAR (60) NOT NULL,
-  alias VARCHAR (30) NOT NULL,
-  publica BOOLEAN NOT NULL DEFAULT FALSE,
-  fecha_creacion DATE NOT NULL DEFAULT CURRENT_DATE,
-  fecha_ultima_mod DATE NOT NULL DEFAULT CURRENT_DATE,
-  descripcion VARCHAR (280) CHECK (descripcion <> ''),
-  PRIMARY KEY (nombre, alias),
-  FOREIGN KEY (alias)
-    REFERENCES usuario (alias)
+CREATE TABLE list (
+  list_name VARCHAR (60) NOT NULL,
+  username VARCHAR (30) NOT NULL,
+  is_public BOOLEAN NOT NULL DEFAULT FALSE,
+  date_created DATE NOT NULL DEFAULT CURRENT_DATE,
+  date_modified DATE NOT NULL DEFAULT CURRENT_DATE,
+  description VARCHAR (280) CHECK (description <> ''),
+  PRIMARY KEY (list_name, username),
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT modificacion_valida
-    CHECK (fecha_creacion <= fecha_ultima_mod)
+    CHECK (date_created <= date_modified)
 );
 
-CREATE TABLE contener (
-  id_entrada INT NOT NULL,
-  nombre_lista_rep VARCHAR (60) NOT NULL,
-  alias VARCHAR (30) NOT NULL,
-  PRIMARY KEY (id_entrada, nombre_lista_rep, alias),
-  FOREIGN KEY (nombre_lista_rep, alias)
-    REFERENCES lista_reproduccion (nombre, alias)
+CREATE TABLE list_movies (
+  id_entry INT NOT NULL,
+  list_name VARCHAR (60) NOT NULL,
+  username VARCHAR (30) NOT NULL,
+  PRIMARY KEY (id_entry, list_name, username),
+  FOREIGN KEY (list_name, username)
+    REFERENCES list (list_name, username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (alias)
-    REFERENCES usuario (alias)
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
--- VALORACIONES Y RESPUESTAS
+-- RATINGS & REPLIES
 
-CREATE TABLE valoracion (
-  id_valoracion SERIAL PRIMARY KEY,
-  id_entrada INT NOT NULL,
-  calificacion INT NOT NULL CHECK (calificacion BETWEEN 1 AND 5),
-  FOREIGN KEY (id_entrada)
-    REFERENCES entrada (id_entrada)
+CREATE TABLE rating (
+  id_rating SERIAL PRIMARY KEY,
+  id_entry INT NOT NULL,
+  username VARCHAR (30) NOT NULL,
+  stars INT NOT NULL CHECK (stars BETWEEN 1 AND 5),
+  message TEXT CHECK (message <> ''),
+  date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (username)
+    REFERENCES users (username)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (id_entry)
+    REFERENCES entry (id_entry)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE respuesta (
-  id_respuesta SERIAL PRIMARY KEY,
-  id_valoracion INT NOT NULL,
-  alias VARCHAR (30) NOT NULL,
-  mensaje VARCHAR (280) CHECK (mensaje <> ''),
-  fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (alias)
-    REFERENCES usuario (alias)
+CREATE TABLE reply (
+  id_reply SERIAL PRIMARY KEY,
+  id_rating INT NOT NULL,
+  username VARCHAR (30) NOT NULL,
+  message TEXT CHECK (message <> ''),
+  date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_valoracion)
-    REFERENCES valoracion (id_valoracion)
+  FOREIGN KEY (id_rating)
+    REFERENCES rating (id_rating)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE evaluar (
-  alias VARCHAR (30) NOT NULL,
-  id_respuesta INT NOT NULL,
-  gustar BOOLEAN NOT NULL,
-  PRIMARY KEY (alias, id_respuesta),
-  FOREIGN KEY (alias)
-    REFERENCES usuario (alias)
+CREATE TABLE likes (
+  username VARCHAR (30) NOT NULL,
+  id_reply INT NOT NULL,
+  is_like BOOLEAN NOT NULL,
+  PRIMARY KEY (username, id_reply),
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_respuesta)
-    REFERENCES respuesta (id_respuesta)
+  FOREIGN KEY (id_reply)
+    REFERENCES reply (id_reply)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE reportar_respuesta (
-  alias VARCHAR (30) NOT NULL,
-  id_respuesta INT NOT NULL,
-  PRIMARY KEY (alias, id_respuesta),
-  FOREIGN KEY (alias)
-    REFERENCES usuario (alias)
+CREATE TABLE report_rating (
+  username VARCHAR (30) NOT NULL,
+  id_rating INT NOT NULL,
+  PRIMARY KEY (username, id_rating),
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_respuesta)
-    REFERENCES respuesta (id_respuesta)
+  FOREIGN KEY (id_rating)
+    REFERENCES rating (id_rating)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
--- CATEGORIAS
-
-CREATE TABLE categoria (
-  nombre VARCHAR (60) PRIMARY KEY CHECK (nombre <> '')
-);
-
-CREATE TABLE categorizar (
-  nombre VARCHAR (60) NOT NULL,
-  id_entrada INT NOT NULL,
-  PRIMARY KEY (nombre, id_entrada),
-  FOREIGN KEY (nombre)
-    REFERENCES categoria (nombre)
+CREATE TABLE report_reply (
+  username VARCHAR (30) NOT NULL,
+  id_reply INT NOT NULL,
+  PRIMARY KEY (username, id_reply),
+  FOREIGN KEY (username)
+    REFERENCES users (username)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (id_entrada)
-    REFERENCES entrada (id_entrada)
+  FOREIGN KEY (id_reply)
+    REFERENCES reply (id_reply)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+-- GENRES
+
+CREATE TABLE genre (
+  name VARCHAR (60) PRIMARY KEY CHECK (name <> '')
+);
+
+CREATE TABLE movie_genre (
+  name VARCHAR (60) NOT NULL,
+  id_entry INT NOT NULL,
+  PRIMARY KEY (name, id_entry),
+  FOREIGN KEY (name)
+    REFERENCES genre (name)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  FOREIGN KEY (id_entry)
+    REFERENCES entry (id_entry)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
