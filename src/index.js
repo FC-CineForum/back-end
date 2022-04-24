d8a249f5b4aed776da1464b3ff582e798b9aff28
CS-Leslie <leslieramg@ciencias.unam.mx>
2022-04-24 11:28:18 -0500

    server config

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 
@@ -12,6 +13,12 @@ console.log('Server is running on port:', PORT);
 
 app.use(morgan('dev'));
 app.use(helmet()); 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
