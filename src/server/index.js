08808c3eaf44a2f2c2d720265f4db7cc6d4bea8b
CS-Leslie <leslieramg@ciencias.unam.mx>
2022-04-27 15:40:46 -0500

    server config

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-const session = require('express-session');
 
 const app = express();
 
@@ -23,25 +22,12 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: `${process.env.COOKIE_SECRET}`,
-    credentials: true,
-    name: 'session',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { 
-      secure: process.env.ENV === 'PRODUCTION',
-      httpOnly: true,
-      sameSite: process.env.ENV === 'PRODUCTION' ? 'none' : 'lax',
-     },
-  })
-);
 
 app.get('/', (_, res) => {
   res.send('Cine-Forum Backend is running!');
 });
 
-app.use('/cineforum', require('./routes'));
+app.use('/cineforum', require('../routes'));
 
-module.exports = { app, PORT };
+
+module.exports = { app };
