aac2e835597014d8b16a5d9a31d4a01d05a46d61
CS-Leslie <leslieramg@ciencias.unam.mx>
2022-05-24 10:22:36 -0500

    database config

---
+++ database/config/index.js
@@ -0,0 +1,14 @@
+const { Client } = require('pg');
+
+const database = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = { database };
