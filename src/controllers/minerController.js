668699788212f73baa66dc1101d9d3fefd28e1b0
garay <frga@ciencias.unam.mx>
2022-06-21 13:20:02 -0500

    adding mining controller

--- src/controllers/minerController.js
+++ src/controllers/minerController.js
@@ -1,7 +1,17 @@
+//import { execaCommand } from "execa";
+
 const { database } = require('../../database/config/index');
 
-const miner = async (req, res) => {}
+//const { execa } = await import("execa");
+
+const miner = async (req, res) => {
+  const { url } = req.body;
+  const execaCommand = (await import('execa')).execaCommand;
+  const command = "python3 ../../../miner/MineBySearch.py 0 "+url;
+  const { stdout } = execaCommand(command);
+  console.log({ stdout });// Code which uses `execa` here.
+}
 
 module.exports = {
-  miner,
+  miner
 }
--- src/validators/minerValidator.js
+++ src/validators/minerValidator.js
@@ -3,7 +3,7 @@ var { Segments, Joi, celebrate } = require('celebrate');
 module.exports = {
   miner: celebrate({
     [Segments.BODY]: Joi.object().keys({
-      url: Joi.string().max(30).required(),
+      url: Joi.string().required(),
     }),
   }),
 }
